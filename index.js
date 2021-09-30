a13e2e5d4928b3bf5baa206eca5b423b1c866d8b
Kelton Hale <keltonhale33@gmail.com>
2021-09-29 21:01:11 -0600

    fetched api and sorted data

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
 
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const api = `https://api.weather.gov/points/${lat},${long}/forecast`;
+
+      fetch(api)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          const { temperature, shortForecast } = data.properties.periods[0];
+
+          // Set DOM Elements from the API
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = shortForecast;
+        });
+      const api2 = `https://api.weather.gov/points/${lat},${long}`;
+      fetch(api2)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          const { city, state } = data.properties.relativeLocation.properties;
+          locationTimezone.textContent = `${city}, ${state}`;
+        });
+    });
+  }
+});
